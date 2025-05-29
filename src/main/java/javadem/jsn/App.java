d039fadabb91070c1868a6a12b4fa8c172410c64
Your Name <you@example.com>
2025-05-29 11:26:11 +0530

    first commit

---
+++ src/main/java/javadem/jsn/App.java
@@ -0,0 +1,13 @@
+package javadem.jsn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
